3b7884059f3a0ad7a83f2c876c439d7569679b68
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2023-11-07 21:45:55 -0500

    Realized I completed 3c Stretch in place of 3b. Moved snippet to 3c and added proper code for 3b

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -45,11 +45,12 @@ console.log('Second animal is: ', secondAnimal);
 //      You'll need to console.log `animalArray` and `lastAnimal` to make
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
-let lastAnimal = animalArray[animalArray.length-1];
+let lastAnimal = animalArray[3];
 console.log('Last animal is: ', lastAnimal);
 // 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
 //       property, rather than the exact (hardcoded) index number of the last item.
-
+lastAnimal = animalArray[animalArray.length-1];
+console.log('Last animal is: ', lastAnimal);
 
 
 // Adding & Removing Array Items
